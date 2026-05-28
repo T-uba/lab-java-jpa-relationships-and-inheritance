44b94ff60419095bf771763eed419fab401cb73a
Tuba <tuba.uzun@outlook.de>
2026-05-28 19:55:32 +0200

    Ironhack Task Week8 New

--- src/main/java/Lab/JPA/Relationships/InheritanceWeek8/NurseAssociationDivisionManagement/Repository/AssociationRepository.java
+++ src/main/java/Lab/JPA/Relationships/InheritanceWeek8/NurseAssociationDivisionManagement/Repository/AssociationRepository.java
@@ -3,6 +3,11 @@ package Lab.JPA.Relationships.InheritanceWeek8.NurseAssociationDivisionManagemen
 import Lab.JPA.Relationships.InheritanceWeek8.NurseAssociationDivisionManagement.Model.Association;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 
-public interface AssociationRepository extends JpaRepository<Association, Long> {}
+
+public interface AssociationRepository extends JpaRepository<Association, Long> {
+
+    List<Association> findByName(Long id);
+}
 
--- src/main/java/Lab/JPA/Relationships/InheritanceWeek8/NurseAssociationDivisionManagement/Repository/DivisionRepository.java
+++ src/main/java/Lab/JPA/Relationships/InheritanceWeek8/NurseAssociationDivisionManagement/Repository/DivisionRepository.java
@@ -1,5 +1,6 @@
 package Lab.JPA.Relationships.InheritanceWeek8.NurseAssociationDivisionManagement.Repository;
 
+import Lab.JPA.Relationships.InheritanceWeek8.NurseAssociationDivisionManagement.Model.Association;
 import Lab.JPA.Relationships.InheritanceWeek8.NurseAssociationDivisionManagement.Model.Division;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,4 +8,6 @@ import java.util.List;
 
 public interface DivisionRepository extends JpaRepository<Division, Long> {
     List<Division> findByDistrict(String district);
+
+    List<Division> findByAssociation(Association association);
 }
